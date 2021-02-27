6c217b1952badbef587e1265a975e7f594765524
MikeCAT <mikecat@users.noreply.github.com>
2021-02-27 09:38:23 +0900

    add JMP after disabling Long Mode

--- 08_x64_test.c
+++ 08_x64_test.c
@@ -6,6 +6,8 @@ void entry(void) {
 		"mov %cr0, %eax\n\t"
 		"and $0x7fffffff, %eax\n\t"
 		"mov %eax, %cr0\n\t"
+		"jmp 1f\n\t" /* instruction set changed, flash pipeline? */
+		"1:\n\t"
 	);
 
 	init_serial_direct();
