74678c56316ecf0ea0c9cc360a2558cffc1fcce3
MikeCAT <mikecat@users.noreply.github.com>
2021-04-06 01:31:08 +0900

    add FONT_WIDTH and FONT_HEIGHT to font.h

--- lib/font.h
+++ lib/font.h
@@ -1,6 +1,9 @@
 #ifndef FONT_H_GUARD_7177776A_3F5B_4C75_81A6_FD1EF12DDD36
 #define FONT_H_GUARD_7177776A_3F5B_4C75_81A6_FD1EF12DDD36
 
+#define FONT_WIDTH 8
+#define FONT_HEIGHT 12
+
 extern unsigned char font_data[256][12];
 
 #endif
